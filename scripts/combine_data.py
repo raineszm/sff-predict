96399dae179b68823939060d4d5446cb949a1646
Zach Raines <dev@zmraines.com>
2025-05-19 21:06:25 -0500

    filter for english and 1959 or after
    Former-commit-id: 2ba0b711386c87eb0e570ddd04eec106c85a550c

--- scripts/combine_data.py
+++ scripts/combine_data.py
@@ -1,20 +1,27 @@
 import polars as pl
 from snakemake.script import snakemake
-import re
 
 RATINGS_THRESHOLD = snakemake.config["filter"]["ratings_threshold"]
 TAG_THRESHOLD = RATINGS_THRESHOLD * snakemake.config["filter"]["tag_threshold"]
 SCIFI_FANTASY_REGEX = r"(?i)fantasy|sci(ence)?-fi"
 
 print("Selecting works with at least {} ratings".format(RATINGS_THRESHOLD))
-raw_works = pl.read_ndjson(snakemake.input["works"]).filter(
-    pl.col("ratings_count").cast(int) >= RATINGS_THRESHOLD,
+raw_works = (
+    pl.scan_ndjson(snakemake.input["works"])
+    .filter(
+        pl.col("ratings_count").cast(int) >= RATINGS_THRESHOLD,
+        pl.col("original_publication_year").cast(int, strict=False) >= 1959,
+    )
+    .collect(engine="streaming")
+)
+
+books = pl.scan_ndjson(snakemake.input["books"], infer_schema_length=1000).filter(
+    pl.col("language_code").str.starts_with("en"),
+    pl.col("work_id").is_in(raw_works["work_id"].to_list()),
+    pl.col("publication_year").cast(int, strict=False) >= 1959,
 )
 
 print("Collecting tags for selected works")
-books = pl.scan_ndjson(snakemake.input["books"], infer_schema_length=1000).filter(
-    pl.col("work_id").is_in(raw_works["work_id"].to_list())
-)
 work_tags = (
     books.group_by("work_id")
     .agg(
@@ -28,21 +35,21 @@ work_tags = (
         )
         .list.any()
     )
-    .collect()
+).collect(engine="streaming")
+
+works = raw_works.join(work_tags, on="work_id", how="inner")
+
+print(
+    "Writing filtered works and tags to {}".format(snakemake.output["filtered_works"])
 )
-
-works = raw_works.join(work_tags, on="work_id", how="left")
-
-print("Writing filtered works and tags {}".format(snakemake.output["filtered_works"]))
 works.write_parquet(snakemake.output["filtered_works"])
 
-best_book_ids = works["best_book_id"].unique().to_list()
-
 print("Combining into one dataset")
-best_book_data = books.filter(pl.col("book_id").is_in(best_book_ids)).collect()
-complete_works = best_book_data.join(works, on="work_id", how="inner")
+complete_works = works.lazy().join(
+    books, left_on="best_book_id", right_on="book_id", how="inner"
+)
 
 print(
     "Writing combined works and tags to {}".format(snakemake.output["combined_works"])
 )
-complete_works.write_parquet(snakemake.output["combined_works"])
+complete_works.sink_parquet(snakemake.output["combined_works"])
