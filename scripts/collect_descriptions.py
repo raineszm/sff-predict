1be35126f48a23f9aade76a5c5c2d58028920339
Zach Raines <dev@zmraines.com>
2025-05-29 21:01:41 -0500

    fetch description data from Google Books
    Former-commit-id: 6c55395a3592da9fbff2c3b0b9194af1cecb23ba

--- scripts/collect_descriptions.py
+++ scripts/collect_descriptions.py
@@ -1,10 +1,13 @@
 from snakemake.script import snakemake
 import duckdb
-from utils.metadata import WikipediaDescriptionProvider
+from utils.metadata import WikipediaDescriptionProvider, GoogleBooksDescriptionProvider
 import numpy as np
 from tqdm.auto import tqdm
 import pandas as pd
-import requests_cache
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 duckdb.sql(
     f"CREATE OR REPLACE TABLE openlibrary_ids AS FROM '{snakemake.input['openlibrary_ids']}'"
@@ -21,6 +24,7 @@ WHERE openlibrary_works.description IS NOT NULL
         ol_works=snakemake.input["ol_works"],
     )
 ).df()
+duckdb.register("openlibrary_descriptions", openlibrary_descriptions)
 
 print(
     "Fetched {} descriptions from openlibrary data".format(
@@ -38,13 +42,7 @@ wikipedia_ids = duckdb.sql(
 
 wikipedia_ids["description"] = np.empty_like(wikipedia_ids.wikipedia_url)
 
-with requests_cache.enabled(
-    backend="filesystem",
-    cache_control=False,
-    cache_name=".cache/wikipedia",
-    expire_after=requests_cache.NEVER_EXPIRE,
-):
-    wiki = WikipediaDescriptionProvider()
+with WikipediaDescriptionProvider() as wiki:
     for i, row in tqdm(
         wikipedia_ids.iterrows(),
         desc="Getting descriptions from wikipedia",
@@ -52,15 +50,49 @@ with requests_cache.enabled(
     ):
         wikipedia_ids.loc[i, "description"] = wiki.get_description(row["wikipedia_url"])
 
-wikipedia_ids = wikipedia_ids.drop(columns=["wikipedia_url"])
+wikipedia_descriptions = wikipedia_ids.dropna().drop(columns=["wikipedia_url"])
+duckdb.register("wikipedia_descriptions", wikipedia_descriptions)
 
 print("Fetched {} descriptions from wikipedia".format(wikipedia_ids.shape[0]))
 
+isbns = duckdb.sql(
+    """
+    SELECT DISTINCT work_qid, isbn
+    FROM '{isbns}'
+    ANTI JOIN openlibrary_descriptions USING (work_qid)
+    ANTI JOIN wikipedia_descriptions USING (work_qid)
+""".format(isbns=snakemake.input["isbns"])
+).df()
+
+with GoogleBooksDescriptionProvider(os.getenv("GOOGLE_BOOKS_API_KEY")) as google_books:
+    work_qids = isbns.work_qid.unique()
+    isbn_descriptions = pd.DataFrame(
+        {
+            "work_qid": work_qids,
+            "description": np.empty_like(work_qids, dtype="object"),
+        }
+    ).set_index("work_qid")
+    isbn_groups = isbns.groupby("work_qid")
+    for work_qid, group in tqdm(
+        isbn_groups,
+        desc="Getting descriptions from google books",
+        total=len(isbn_groups),
+    ):
+        for isbn in group["isbn"]:
+            if description := google_books.get_description(isbn):
+                isbn_descriptions.loc[work_qid, "description"] = description
+                break
+
+
+isbn_descriptions = isbn_descriptions.dropna().reset_index()
+
+print("Fetched {} descriptions from google books".format(isbn_descriptions.shape[0]))
+
 
 descriptions = (
-    pd.concat([openlibrary_descriptions, wikipedia_ids])
-    .dropna(subset=["description"])
-    .drop_duplicates(subset=["work_qid"])
+    pd.concat(
+        [openlibrary_descriptions, wikipedia_descriptions, isbn_descriptions]
+    ).drop_duplicates(subset=["work_qid"])
 ).set_index("work_qid")
 
 n_works = duckdb.sql(
