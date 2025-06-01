34580ef05eda497cc860332926af7f945b634fe5
Zach Raines <dev@zmraines.com>
2025-06-01 15:10:24 -0500

    progress bar for embedding
    This takes a while for the headlines so it's nice to know what's happenign
    
    
    Former-commit-id: d52fbabb12fba8bc0b7a56bb04998a346d30618a

--- scripts/embed_headlines.py
+++ scripts/embed_headlines.py
@@ -1,15 +1,21 @@
 from snakemake.script import snakemake
 import duckdb
 from utils.embedding import register_embedding_function
-
-register_embedding_function()
+from tqdm.auto import tqdm
 
 print("Embedding headlines (This may take a while)")
 
-duckdb.sql(f"""
-COPY (
-SELECT id, embed_text(headline || ' ' || abstract) AS embedding
-FROM '{snakemake.input["headlines"]}'
-)
-TO '{snakemake.output["headline_embeddings"]}' (FORMAT PARQUET, COMPRESSION ZSTD)
-""")
+n_headlines = duckdb.sql(
+    f"SELECT COUNT(*) FROM '{snakemake.input['headlines']}'"
+).fetchone()[0]
+
+with tqdm(total=n_headlines, desc="Embedding headlines") as total_pbar:
+    register_embedding_function(total_pbar=total_pbar)
+
+    duckdb.sql(f"""
+    COPY (
+    SELECT id, embed_text(headline || ' ' || abstract) AS embedding
+    FROM '{snakemake.input["headlines"]}'
+    )
+    TO '{snakemake.output["headline_embeddings"]}' (FORMAT PARQUET, COMPRESSION ZSTD)
+    """)
