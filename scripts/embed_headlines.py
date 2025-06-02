c3c9eca2c5787616e0d5802b4cf815038cc222e9
Zach Raines <dev@zmraines.com>
2025-06-01 21:49:17 -0500

    perform sentiment analysis with pretrained model
    currently using finedtuned distilbert model
    
    
    Former-commit-id: 9ab66172350aa15bbbc3b5597d0beaa1c5d4b4ee

--- scripts/embed_headlines.py
+++ scripts/embed_headlines.py
@@ -1,21 +1,55 @@
-from snakemake.script import snakemake
 import duckdb
+import sys
+
+sys.path.append(".")
+
 from utils.embedding import register_embedding_function
+from utils.sentiment import register_sentiment_function
 from tqdm.auto import tqdm
 
-print("Embedding headlines (This may take a while)")
 
-n_headlines = duckdb.sql(
-    f"SELECT COUNT(*) FROM '{snakemake.input['headlines']}'"
-).fetchone()[0]
+def embed_headlines(input_headlines, output_headline_embeddings):
+    print("Embedding headlines (This may take a while)")
 
-with tqdm(total=n_headlines, desc="Embedding headlines") as total_pbar:
-    register_embedding_function(total_pbar=total_pbar)
+    n_headlines = duckdb.sql(f"SELECT COUNT(*) FROM '{input_headlines}'").fetchone()[0]
 
-    duckdb.sql(f"""
-    COPY (
-    SELECT id, embed_text(headline || ' ' || abstract) AS embedding
-    FROM '{snakemake.input["headlines"]}'
+    with tqdm(total=n_headlines, desc="Embedding headlines") as total_pbar:
+        register_sentiment_function()
+        register_embedding_function(total_pbar=total_pbar)
+
+        # Strip the nyt://article/ prefix from the id
+        # and combine the headline and abstract for embedding
+        duckdb.sql(f"""
+        CREATE VIEW concat_headline_abstract AS (
+            SELECT id.substr(strlen('nyt://article/') + 1) as id, headline || ' ' || abstract AS headline_abstract
+            FROM '{input_headlines}'
+        );
+        """)
+
+        # Do vectorized semanticembedding and sentiment analysis
+        # of the headlines
+        duckdb.sql(f"""
+        COPY (
+        SELECT id, embed_text(headline_abstract) AS embedding, sentiment(headline_abstract) AS sentiment
+        FROM concat_headline_abstract
+        )
+        TO '{output_headline_embeddings}' (FORMAT PARQUET, COMPRESSION ZSTD)
+        """)
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 3:
+        print(
+            "Usage: python embed_headlines.py <input_headlines> <output_headline_embeddings>"
+        )
+        sys.exit(1)
+
+    embed_headlines(sys.argv[1], sys.argv[2])
+else:
+    from snakemake.script import snakemake
+
+    embed_headlines(
+        snakemake.input["headlines"], snakemake.output["headline_embeddings"]
     )
-    TO '{snakemake.output["headline_embeddings"]}' (FORMAT PARQUET, COMPRESSION ZSTD)
-    """)
