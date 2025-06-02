656bbce790ec994542d6984095af7de910fe449f
Zach Raines <dev@zmraines.com>
2025-06-01 22:20:28 -0500

    perform sentiment analysis with pretrained model
    currently using finedtuned distilbert model
    
    
    Former-commit-id: 9fd8d18863ea4667af464f9dcb0b37953f4e1085

---
+++ utils/sentiment.py
@@ -0,0 +1,33 @@
+import duckdb
+import pyarrow as pa
+from transformers import pipeline
+from typing import Optional
+from tqdm.auto import tqdm
+
+
+def assign_sentiment(
+    text: pa.Array, classifier: pipeline, total_pbar: Optional[tqdm] = None
+) -> pa.Array:
+    sentiments = [t["label"] for t in classifier(text.to_pylist())]
+    if total_pbar is not None:
+        total_pbar.update(len(sentiments))
+    return pa.array(sentiments, type=pa.string())
+
+
+def register_sentiment_function(total_pbar: Optional[tqdm] = None):
+    # We use distilbert with transformers to get a sentiment score
+    classifier = pipeline(
+        "sentiment-analysis",
+        model="distilbert-base-uncased-finetuned-sst-2-english",
+        revision="714eb0f",
+        truncation=True,
+        max_length=512,
+    )
+
+    duckdb.create_function(
+        "sentiment",
+        lambda x: assign_sentiment(x, classifier, total_pbar=total_pbar),
+        parameters=[duckdb.typing.VARCHAR],
+        return_type=duckdb.typing.VARCHAR,
+        type="arrow",
+    )
