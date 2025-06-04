f6cc3be304eeb7f094c09b38beeb65c6a05bc150
Zach Raines <dev@zmraines.com>
2025-06-03 20:21:18 -0500

    snakemake touch cached files

--- bin/fetch_cache.sh
+++ bin/fetch_cache.sh
@@ -5,17 +5,13 @@ set -euo pipefail
 echo "Fetching prefetched NYT headlines and embeddings..."
 TMPDIR="$(mktemp -d)"
 if [[ ! -f "data/raw/headlines.json" ]] || [[ ! -f "data/headline_embeddings.parquet" ]]; then
-    pixi run kaggle datasets download -d zacharymraines/sff-project-data-cache --path "$TMPDIR" --unzip
-    mv "$TMPDIR/headlines.json" "data/raw/headlines.json" 
-    mv "$TMPDIR/headline_embeddings.parquet" "data/headline_embeddings.parquet"
+	pixi run kaggle datasets download -d zacharymraines/sff-project-data-cache --path "$TMPDIR" --unzip
+	mv "$TMPDIR/headlines.json" "data/raw/headlines.json"
+	mv "$TMPDIR/headline_embeddings.parquet" "data/headline_embeddings.parquet"
 fi
 
 echo "Generating monthly headlines, so that Snakemake is happy"
 pixi run python -m scripts.split_cached_headlines
-for file in data/raw/headlines/*.json; do
-    touch "$file"
-done
-touch "data/raw/headlines.json"
-touch "data/headline_embeddings.parquet"
+bin/snakemake.sh --quiet all --touch data/raw/headlines/*.json data/raw/headlines.json data/headline_embeddings.parquet
 
 echo "Done!"
