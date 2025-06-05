bee023677562d68f796948affdc7edf45ca3f6cd
Zach Raines <dev@zmraines.com>
2025-06-04 21:42:46 -0500

    select for "peaky" topics

--- models/topics.py
+++ models/topics.py
@@ -3,27 +3,50 @@ from sentence_transformers import SentenceTransformer
 from bertopic import BERTopic
 from bertopic.representation import KeyBERTInspired
 from sklearn.metrics.pairwise import cosine_similarity
+from scipy.stats import kurtosis
 
 
 class TopicExtractionModel:
-    def __init__(self, embedding_model: SentenceTransformer, top_n_topics: int = 20):
+    def __init__(
+        self,
+        embedding_model: SentenceTransformer,
+        monthly_threshold_frequency: int = 10,
+        kurtosis_threshold: float = 2,
+    ):
+        self.monthly_threshold_frequency = monthly_threshold_frequency
+        self.kurtosis_threshold = kurtosis_threshold
         self.model = BERTopic(
             language="english",
             representation_model=KeyBERTInspired(),
             embedding_model=embedding_model,
             calculate_probabilities=False,
         )
-        self.top_n_topics = top_n_topics
-        self.top_n_topic_embeddings = None
 
-    def fit(self, texts: list[str], embeddings: np.ndarray):
+    def fit(self, texts: list[str], embeddings: np.ndarray, months: list[int]):
         self.model.fit(texts, embeddings)
-        self.centroids = self.model.topic_embeddings_[1 : self.top_n_topics + 1, :]
-        topic_sizes = np.array(
-            [self.model.topic_sizes_[i] for i in range(self.top_n_topics)]
+
+        self.topics_over_time = self.model.topics_over_time(
+            texts,
+            months,
+            global_tuning=False,
+            evolution_tuning=False,
         )
+
+        kt = (
+            self.topics_over_time[
+                self.topics_over_time["Frequency"] >= self.monthly_threshold_frequency
+            ]
+            .sort_values("Timestamp")
+            .groupby("Topic")
+            .Frequency.agg(lambda x: kurtosis(x, bias=False, fisher=True))
+        )
+        hot_topics = kt[kt > self.kurtosis_threshold].index
+
+        self.centroids = self.model.topic_embeddings_[hot_topics]
+        topic_sizes = np.array([self.model.topic_sizes_[i] for i in hot_topics])
         self.weights = topic_sizes / topic_sizes.sum()
         return self
 
     def compute_alignment(self, embeddings: np.ndarray) -> np.array:
-        return cosine_similarity(embeddings, self.centroids).dot(self.weights)
+        similarities = cosine_similarity(embeddings, self.centroids)
+        return (1 + similarities).sum(axis=1)
